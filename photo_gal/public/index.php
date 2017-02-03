00f46ff90abd3f11e8b6dfc22fa517883f888039
mbrick02 <mbrick02@yahoo.com>
2017-02-03 13:56:15 -0500

    new Centos7 MainDesktop

--- photo_gal/public/index.php
+++ photo_gal/public/index.php
@@ -4,5 +4,5 @@ require_once("../includes/database.php");
 if(isset($database)) { echo "db!"; } else { echo "**No db!"; }
 echo "<br />";
 
-echo "Is this working?";
+echo "Is this 020317 working?";
 ?>
