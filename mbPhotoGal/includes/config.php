63f66db5c63fd9c769efb2cd1a2df33ce03ddbae
mbrick02 <mbrick02@yahoo.com>
2017-02-03 12:40:56 -0500

    delet

--- mbPhotoGal/includes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Database Constants
-if(!defined('DB_SERVER')) {define("DB_SERVER", "localhost");}
-defined('DB_USER') ? null : define("DB_USER", "michael");
-defined('DB_PASS') ? null : define("DB_PASS",  "Job4Fau");
-defined('DB_NAME') ? null : define("DB_NAME", "photo_gallery");
-defined('DB_CHAR') ? null : define("DB_CHAR", "utf8");
-?>
