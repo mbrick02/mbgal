900a29078e3f8cd2bc25bed294a742eb6334411e
Michael Brickler <mbrick02@yahoo.com>
2017-03-25 14:20:22 -0400

    untested updates to list_photos & list_photo_comments

--- photo_gal/public/admin/list_photo_comments.php
+++ photo_gal/public/admin/list_photo_comments.php
@@ -1,8 +1,10 @@
 // **** FROM photo.php  BELOW IS code from list_photos
 // need to get the photo id to show the image and find all the comments 
 <?php
-require_once("../includes/initialize.php");
+require_once("../../includes/initialize.php");
 require_once(LIB_PATH.DS."comment.php");
+
+if(!$session->is_logged_in()) { redirect_to("login.php"); }
 
 if(empty($_GET['id'])) {
 	$session->message("No photograph ID was provided.");
@@ -15,24 +17,6 @@ if (!$photo) {
 	redirect_to('index.php');
 }
 
-if(isset($_POST['submit'])) {
-	$author = trim($_POST['author']);
-	$body = trim($_POST['body']);
-	
-	$new_comment = Comment::make($photo->id, $author, $body);
-	if($new_comment && $new_comment->save()) {
-		// was saved -- no $message nessary, because comment will now show
-		
-		// Don't render (or could resubmit same values) instead...
-		redirect_to("photo.php?id={$photo->id}");
-	} else {
-		// Failed
-		$message = "There was an error that prevented the comment from being saved.";
-	}
-} else {
-	$author = "";
-	$body = "";
-}
 
 $comments = $photo->comments();
 ?>
@@ -63,19 +47,7 @@ $comments = $photo->comments();
 	<?php if(empty($comments)) { echo "No Comments."; } ?>
 </div>
 
-
-<?php include_layout_template("footer.php") ?>
-
-// ****FROM list_photos.... ***************************************************************
-<?php
-require_once("../../includes/initialize.php");
-
-if(!$session->is_logged_in()) { redirect_to("login.php"); }
-?>
-<?php // not needed, $message cleared in session; $message =""; ?>
-<?php include_layout_template("admin_header.php") ?>
-<h2>List of Photos</h2>
-<?php $photosAry = Photograph::find_all(); ?>
+<h2>Comments on Photo</h2>
 <?php echo output_message($message); ?>
 <table class="bordered">
 <tr>
@@ -94,12 +66,19 @@ foreach ($comments as $comment) { // or use foreach($phs as $ph): format (no cur
 	echo "<td>". $comment->author . "</td>";
 	echo "<td>". $comment->body . "</td>";
 	echo "<td>". $comment->date . "</td>";
-	echo "<td><a href=\"delete_comment.php?id=" . $photo->id . "\">Delete</a></td>";
+	echo "<td><a href=\"delete_comment.php?id=" . $comment->id . "\">Delete</a></td>";
 	echo "</tr>";
 }
 ?>
 </table>
-<br>
+<br />
+
+<?php include_layout_template("footer.php") ?>
+
+// ****FROM list_photos.... ***************************************************************
+
+<?php // not needed, $message cleared in session; $message =""; ?>
+
 <a href="photo_upload.php">Upload a new photo</a>
 
 <?php include_layout_template("admin_footer.php") ?>
